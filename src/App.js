18e818b76b0cf79b6b3a310f05bc4bf55a1aa4f1
Ditochoza <35265732+Ditochoza@users.noreply.github.com>
2022-05-06 21:57:28 +0200

    Añadida comprobación conexión

--- src/App.js
+++ src/App.js
@@ -11,29 +11,33 @@ export default class App extends Component {
   }
 
   searchBooks = (title, authors, printType) => {
-    this.setState({loading: true});
-    var queryString = "";
-    if (title !== '') queryString = "intitle:" + title.trim().replace(" ", "+");
-    if (authors !== '' && printType !== "magazines") {
-        if (queryString !== '') queryString += "&&";
-        queryString += "inauthor:" + authors.replace(" ", "+");
-    }
-    if (queryString !== '') queryString = "q=" + queryString;
+    if (navigator.onLine) {
+      this.setState({loading: true});
+      var queryString = "";
+      if (title !== '') queryString = "intitle:" + title.trim().replace(" ", "+");
+      if (authors !== '' && printType !== "magazines") {
+          if (queryString !== '') queryString += "&&";
+          queryString += "inauthor:" + authors.replace(" ", "+");
+      }
+      if (queryString !== '') queryString = "q=" + queryString;
 
-    console.log(title, authors, printType)
-    axios.get("https://www.googleapis.com/books/v1/volumes?" + queryString + "&printType=" + printType + "&maxResults=40")  
-            .then(data => {
-                if (data.data.totalItems > 0) {
-                  console.log(data.data.items)
-                  this.setState({loading: false});
-                  this.setState({books : data.data.items})
-                  localStorage.setItem('books', JSON.stringify(data.data.items))
-                } else {
-                  this.setState({loading: false});
-                  this.setState({books : ''})
-                  localStorage.removeItem('books')
-                }
-            }) 
+      console.log(title, authors, printType)
+      axios.get("https://www.googleapis.com/books/v1/volumes?" + queryString + "&printType=" + printType + "&maxResults=40")  
+              .then(data => {
+                  if (data.data.totalItems > 0) {
+                    console.log(data.data.items)
+                    this.setState({loading: false});
+                    this.setState({books : data.data.items})
+                    localStorage.setItem('books', JSON.stringify(data.data.items))
+                  } else {
+                    this.setState({loading: false});
+                    this.setState({books : ''})
+                    localStorage.removeItem('books')
+                  }
+              }) 
+      } else {
+        alert('You cannot search without connection')
+      }
   }
 
   render() {
