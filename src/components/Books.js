a47d4029837bc70ac93c2d2f3a3e93fcf95d61d6
Ditochoza <35265732+Ditochoza@users.noreply.github.com>
2022-05-06 20:47:52 +0200

    Prueba 3 LocalStorage

--- src/components/Books.js
+++ src/components/Books.js
@@ -3,9 +3,10 @@ import Book from './Book'
 
 export default class Books extends Component {
     render() {
-        return this.props.books !== '' ? 
-            this.props.books.map(book => <Book  key={book.id} book={book.volumeInfo}/>) : 
-            <p className='BooksNotFound'>Books not found</p>
+        console.log(this.props.books)
+        return this.props.books === '' ? 
+            <p className='BooksNotFound'>Books not found</p> :
+            this.props.books.map(book => <Book  key={book.id} book={book.volumeInfo}/>)
         
     }
 }
